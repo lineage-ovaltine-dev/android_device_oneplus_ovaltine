3a61e06269a6291ef3ebc426660c167ac5279fbc
Bruno Martins <bgcngm@gmail.com>
2023-12-05 14:20:29 +0000

    ovaltine: Setup firmware extraction
    Change-Id: I47edc759376ca6a897d181a3e8fbc84a4fe22a43
    Signed-off-by: Weikai Kong <priv@pppig236.com>

--- extract-files.py
+++ extract-files.py
@@ -161,7 +161,7 @@ module = ExtractUtilsModule(
     namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
-    add_firmware_proprietary_file=False,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':
