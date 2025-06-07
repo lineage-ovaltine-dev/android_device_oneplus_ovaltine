c8e17a9a4bd0f7171a6f677d7f81b31d0875d39c
pjgowtham <pjgowtham@gmail.com>
2025-06-07 21:54:54 +0530

    ovaltine: Use oplus udfps
    Signed-off-by: Weikai Kong <priv@pppig236.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,9 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_4630947180710147970.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947180710147969.xml
 
+# Fingerprint
+$(call soong_config_set,qtidisplay,oplus_udfps,true)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
